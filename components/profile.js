3f1171de96f8538d0c9c23dfa479641795ab83c4
Lauren Pittenger <lepittenger@gmail.com>
2021-04-14 14:24:40 -0400

    Everything after all the lessons (sorry)

---
+++ components/profile.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+const Profile = () => (
+    <Image
+        src="/images/lauren-pittenger_800x800.jpg"
+        height={400}
+        width={400}
+        alt="Lauren Pittenger"
+    />
+)
